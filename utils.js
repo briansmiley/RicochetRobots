0a8407b4b674d84e441459a15f958d0cdb86f338
Brian Smiley <briantsmiley42@gmail.com>
2024-05-20 13:25:43 -0400

    adds spiralboard function

--- utils.js
+++ utils.js
@@ -6,8 +6,20 @@ function pushWrap(fn) {
     return res;
   };
 }
+
 const colorToBrightness = pushWrap((oldColor, newBrightness) => {
   colorMode(HSB);
   let newColor = color(oldColor);
   return color([hue(newColor), saturation(newColor), newBrightness]);
 });
+
+function spiralBoard(startFrame = 0) {
+  let t = frameCount - startFrame;
+  if (t < 0) return;
+  let xCenter = board.origX + (board.w * squareSize) / 2;
+  let yCenter = board.origY + (board.h * squareSize) / 2;
+  translate(xCenter, yCenter);
+  rotate(t ** 1.5 / 250);
+  scale(0.997 ** t);
+  translate(-xCenter, -yCenter);
+}
